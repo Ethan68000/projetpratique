f53907e02ad834dd4c71d00fc4c13a5ea86a4716
Ethan68000 <cathycoulon910@gmail.com>
2025-02-06 17:18:59 +0100

    modif

---
+++ connexion/inscription.js
@@ -0,0 +1,9 @@
+function inscription(){
+    let nom = parseInt(document.getElementById("nom").value);
+    let prenom = parseInt(document.getElementById("prenom").value);
+    let identifiant = parseInt(document.getElementById("identifiant").value);
+    let email = parseInt(document.getElementById("email").value);
+    let telephone = parseInt(document.getElementById("téléphone").value);
+    let mdp = parseInt(document.getElementById("mdp").value);
+    let confmdp = parseInt(document.getElementById("Confirmationmdp").value);
+}
